fe4027db4868513e3892c4c193153ba196b79e68
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-06-14 23:57:09 +0200

    h

--- hw5/settings.py
+++ hw5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!!tatg)v$6=2gwj6%!ru9-1*f&r9lqub%4%%dj6^p0eo5un_n-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andrejtodorovski5.pythonanywhere.com']
 
 
 # Application definition
